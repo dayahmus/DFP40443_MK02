5d7ae58f10637cb0cdcd9d8822bff05872e93f11
dayahmus <18dit24f1994@student.ptss.edu.my>
2026-01-20 18:27:30 +0800

    pengasingan code

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Biodata</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<div class="container">
+  <h1>Selamat Datang</h1>
+  <?php include 'biodata.php'; ?>
+</div>
+
+</body>
+</html>
